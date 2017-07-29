6096a48d2944e96264547a1f23ed8f06fd1885c5
camel <a556622821@gmail.com>
2017-07-29 11:19:20 +0800

    Add lint, build, ignore file
    Add eslint
    Add .gitignore
    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/* global __dirname */
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    nanocookie: "./src/nanocookie.js"
+  },
+  output: {
+    filename: "./dist/[name].min.js",
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader"],
+        include: path.resolve(__dirname, "assets/src/")
+      }
+    ]
+  },
+};
